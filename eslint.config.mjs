f2cb8deb7dab3f4673121265146d0f52d60d446a
Nguyễn Gia Bảo <ngbaodev@gmail.com>
2024-07-19 10:49:10 +0700

    Migrate eslint v8 to v9

---
+++ eslint.config.mjs
@@ -0,0 +1,56 @@
+import eslintPlugin from '@typescript-eslint/eslint-plugin';
+import prettier from 'eslint-plugin-prettier';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: ['**/.eslintrc.js'],
+  },
+  ...compat.extends(
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ),
+  {
+    plugins: {
+      '@typescript-eslint': eslintPlugin,
+      prettier,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: 'module',
+
+      parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+
+    rules: {
+      'prettier/prettier': ['error', {}],
+      '@typescript-eslint/interface-name-prefix': 0,
+      '@typescript-eslint/explicit-function-return-type': 2,
+      '@typescript-eslint/explicit-module-boundary-types': 1,
+      '@typescript-eslint/no-explicit-any': 2,
+    },
+  },
+];
